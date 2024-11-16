3b962b20bd828ab43119929e69353631ac5370ab
Sultan Al Fadhli <sultanwf04@gmail.com>
2024-11-16 22:31:50 +0300

    Initial commit

---
+++ youtube-playlist.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By                                            
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--disable-features=OptimizationGuideModelDownloading")
+
+service = Service(ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=service)
+
+
+# Playlist INPUT
+#playlist = input("Enter your playlist: ")
+# https://www.youtube.com/playlist?list=PLLs_ak7laW3YVCPOlDQaOzt7Poi1kRDW8
+# https://www.youtube.com/playlist?list=PLLs_ak7laW3bquNeO56dtfyPOznImNCpe
+playlist_url = ("https://www.youtube.com/playlist?list=PLLs_ak7laW3YVCPOlDQaOzt7Poi1kRDW8")
+driver.get(playlist_url)
+
+    # Playlist Informations
+# Playlist NAME
+playlist_name_elements = driver.find_element(By.XPATH, '//h1[@class="dynamic-text-view-model-wiz__h1"]')
+playlist_name = playlist_name_elements.text
+# Playlist CREATOR
+creator_name_element = driver.find_element(By.XPATH, '//a[contains(@href, "/@")]')
+creator_name = creator_name_element.text.strip()
+
+# Playlist Video Informations
+video_elements = driver.find_elements(By.XPATH, '//a[@id="video-title"]')
+
+print(f"{playlist_name} {creator_name}. Videos: {len(video_elements)}")
+
+for index, video in enumerate(video_elements, start=1):
+    video_title = video.get_attribute("title")
+    video_url = video.get_attribute("href")
+    print(f"{index}/ {video_title}")
+
+
+
+time.sleep(5)
+driver.quit()
